3bb50a2041f96e84e18bbc8bc40e43f099d0c571
armand-07 <armand.de.asis@estudiantat.upc.edu>
2024-01-10 10:37:04 +0000

    added the face recognition

--- src/models/inference_face_detection_model.py
+++ src/models/inference_face_detection_model.py
@@ -11,14 +11,24 @@ def detect_faces_HAAR_cascade(img, pretrained_model):
 
     return faces_bbox
 
-def detect_faces_YOLO(img, pretrained_model):
+def detect_faces_YOLO(img, pretrained_model, format = 'xywh', verbose=False):
     """ Detects faces in an image using the given YOLO pretrained model. 
-    Returns in standard bbox format: [x, y, w, h], where x,y are the coordinates of the top-left corner.
+    Returns in standard bbox format: [x, y, w, h], where x,y are the coordinates of the top-left corner. And the condifece of the detection.
     """
-    results = pretrained_model(img)
+    results = pretrained_model.predict(img, verbose = verbose)
 
     # Extract the bounding boxes and probabilities for the image
-    boxes = results[0].boxes
-    probs = results[0].probs
+    if format == 'xywh-center':
+        boxes = results[0].boxes.xywh.cpu().numpy()
+    elif format == 'xywh':
+        boxes = results[0].boxes.xywh.cpu().numpy()
+        boxes[:, 0] = boxes[:, 0] - boxes[:, 2]/2
+        boxes[:, 1] = boxes[:, 1] - boxes[:, 3]/2
+    elif format == 'xyxy':
+        boxes = results[0].boxes.xyxy.cpu().numpy()
+    else:
+        raise ValueError('Format not supported')
+    
+    conf = results[0].boxes.conf.cpu().numpy()
 
-    return [boxes, probs]
+    return [boxes, conf]
--- src/visualization/display_annot.py
+++ src/visualization/display_annot.py
@@ -8,6 +8,63 @@ from random import randint
 from PIL import Image
 
 
+
+def plot_bbox_annotations(img, bbox_annot, format ="xywh", conf_threshold = 0, conf = None,  other_annot = None):
+    """ Displays the bounding boxes and text annotations on the image. The standard format is xywh
+    """
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)      # Convert BGR to RGB
+    height, width, _ = img.shape
+    # Define the parameters for the rectangle
+    background_color = (0, 255, 0) # Green color in RGB
+    text_color = (0, 0, 0) # White color in RGB
+
+    for i in range(len(bbox_annot)):
+        if conf is not None:
+            annot_conf = conf[i]
+        else:
+            annot_conf = 1
+        if annot_conf > conf_threshold:
+            if format == "xywh":
+                [x, y, w, h] = bbox_annot[i].astype(int)
+            elif format == "xywh-center":
+                [x, y, w, h] = bbox_annot[i].astype(int)
+                x = x-int(w/2); 
+                y = y-int(h/2)
+            elif format == "xyxy":
+                [x, y, x2, y2] = bbox_annot[i].astype(int)
+                w = x2 - x
+                h = y2 - y
+            else:
+                raise ValueError("The format of the bounding box is not valid. It must be 'xywh', 'xywh-center' or 'xyxy'")
+            if other_annot is not None:
+                text = other_annot[i]
+            else:
+                text = ""
+            text = str(i)+":"+text
+
+            cv2.rectangle(img, (x, y), (x+w, y+h), background_color, 2)
+
+            # Set font_size and bbox_thickness as a fraction of the image's width
+            font_scale = width / 1000  # Adjust the denominator to get the desired font size
+            bbox_thickness = int(round(width / 200))# Adjust the denominator to get the desired thickness
+            bbox_thickness = max(1, bbox_thickness) # Make sure bbox_thickness is at least 1
+
+            # For the text background
+            # Finds space required by the text so that we can put a background with that amount of width.
+            
+            (w_text, h_text), _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_DUPLEX , font_scale, bbox_thickness)
+            # Prints the text.    
+            img = cv2.rectangle(img, (x, y - int(h_text*1.5)), (x + w_text, y), background_color, -1)
+            img = cv2.putText(img, text, (x, y - 5),
+                                cv2.FONT_HERSHEY_SIMPLEX, font_scale, text_color, int(bbox_thickness*0.75))
+
+    # Displaying the image  
+    plt.imshow(img)  
+    plt.axis('off')
+    plt.show()
+
+
+
 def display_img_annot (sample_df, bbox_thickness = 2, font_size = 0.6):
     sample_path = Path(os.path.join(INTERIM_DATA_DIR,'images', sample_df['path']))
     print("The path of the example image is:", sample_path)
@@ -43,12 +100,10 @@ def display_img_annot (sample_df, bbox_thickness = 2, font_size = 0.6):
 
         # Window name in which image is displayed
         img = cv2.rectangle(img, (x1, y1), (x2, y2), color, bbox_thickness) 
-        print((x1, y1), (x2-x1, y2-y1))
         
         # For the text background
         # Finds space required by the text so that we can put a background with that amount of width.
         (w, h), _ = cv2.getTextSize(str(person_id), cv2.FONT_HERSHEY_DUPLEX , font_size, int(bbox_thickness*0.5))
-
         # Prints the text.    
         img = cv2.rectangle(img, (x1, y1 - int(h*1.5)), (x1 + w, y1), color, -1)
         img = cv2.putText(img, str(person_id), (x1, y1 - 5),
@@ -64,6 +119,7 @@ def display_img_annot (sample_df, bbox_thickness = 2, font_size = 0.6):
 
     # Displaying the table
     print(annotations_table)
+
 
     
 def display_img_set (grid_size, images, title = None):
