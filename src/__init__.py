5e43150342033a01d9835eec1bcf3d7d4683b8d8
armand-07 <armand.de.asis@estudiantat.upc.edu>
2024-01-12 12:51:37 +0000

    Data exploration AffectNet

--- src/__init__.py
+++ src/__init__.py
@@ -7,6 +7,7 @@ ROOT_DIR = Path(Path(__file__).resolve().parent.parent)
 MODELS_DIR = os.path.join(ROOT_DIR, "models")
 RAW_DATA_DIR = os.path.join(ROOT_DIR, "data", "raw")
 INTERIM_DATA_DIR = os.path.join(ROOT_DIR, "data", "interim")
+RAW_AffectNet_DIR = os.path.normpath("/mnt/gpid08/datasets/affectnet/") # substitute with the actual data path
 
 
 
@@ -15,3 +16,6 @@ INTERIM_COLUMNS = ['path','orig_db', 'img_size', 'people', 'bbox', 'label_cat', 
 
 # Define columns for the processed labels
 PROCESSED_COLUMNS = ['face_photo_tensor', 'label_cat', 'label_cont']
+
+# Define the corresponding categorical emotions of AffectNet
+AFFECTNET_CAT_EMOT = ["Neutral", "Happy", "Sad", "Surprise", "Fear", "Disgust", "Anger", "Contempt"] # 0: Neutral, 1: Happy, 2: Sad, 3: Surprise, 4: Fear, 5: Disgust, 6: Anger, 7: Contempt
