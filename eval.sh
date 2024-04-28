990a7f6dcc1914a5a6306d4a3e384d9f45b72a86
armand-07 <arm.deasis1@gmail.com>
2024-04-28 19:12:45 +0200

    new file:   config_deit_small_posterV2_hyperparameter_tunning_weighted_loss.yaml 	modified:   e_sweep_train.sh 	modified:   eval.sh 	modified:   src/models/train_model.py

--- eval.sh
+++ eval.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH -c 2
+#SBATCH --nodelist=gpic11
 #SBATCH --mem 32G
 #SBATCH --gres=gpu:1
 #SBATCH --time 0:15:00 
-python -m src.models.eval_model --wandb_id deep-sweep-6
+python -m src.models.eval_model --wandb_id copper-sweep-3
